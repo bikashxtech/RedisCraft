8f3253228310c25ab31636c6c47a83ab1211c202
bikashxtech <bikashkumargiri23@gmail.com>
2025-08-27 17:28:37 +0530

    Added feature for XADD Command ID auto-generation

--- src/StreamIDVerifier.cpp
+++ src/StreamIDVerifier.cpp
@@ -1,12 +1,18 @@
 #include "StreamIDVerifier.hpp"
 
 bool parse_entry_id(const std::string& id, uint64_t& ms_time, uint64_t& seq_num) {
-    static std::regex id_regex(R"(^(\d+)-(\d+)$)");
+    static std::regex id_regex(R"(^(\d+)-(\*|\d+)$)");
     std::smatch match;
-    if (!std::regex_match(id, match, id_regex)) return false;
+    if (!std::regex_match(id, match, id_regex)) {
+        return false;
+    }
     try {
         ms_time = std::stoull(match[1].str());
-        seq_num = std::stoull(match[2].str());
+        if (match[2].str() == "*") {
+            seq_num = UINT64_MAX;  // mark wildcard internally
+        } else {
+            seq_num = std::stoull(match[2].str());
+        }
     } catch (...) {
         return false;
     }
